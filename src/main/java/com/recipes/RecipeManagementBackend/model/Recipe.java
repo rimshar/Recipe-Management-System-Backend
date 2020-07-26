6b3e6ce41b2e9a29c029df3794602ee4a3fef5f9
Jay Vilkajs <vilkajs.janis@gmail.com>
2020-07-26 13:58:10 +0300

    Created Recipe entity

---
+++ src/main/java/com/recipes/RecipeManagementBackend/model/Recipe.java
@@ -0,0 +1,64 @@
+package com.recipes.RecipeManagementBackend.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String instructions;
+
+    @Column
+    private String link;
+
+    @Column(name = "submittedBy")
+    @ManyToOne
+    private User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
