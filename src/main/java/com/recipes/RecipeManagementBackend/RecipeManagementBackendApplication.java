b9208a21cc846a337af7ca6c103cf6a634e8a58e
Jay Vilkajs <vilkajs.janis@gmail.com>
2020-07-26 12:08:39 +0300

    first commit

---
+++ src/main/java/com/recipes/RecipeManagementBackend/RecipeManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.recipes.RecipeManagementBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeManagementBackendApplication.class, args);
+	}
+
+}
