50f66b4eb0b777fb062b5d9a2a4910989dc0d006
Jay Vilkajs <vilkajs.janis@gmail.com>
2020-07-26 14:18:53 +0300

    Updated Recipe entity

--- src/main/java/com/recipes/RecipeManagementBackend/model/Recipe.java
+++ src/main/java/com/recipes/RecipeManagementBackend/model/Recipe.java
@@ -7,7 +7,7 @@ public class Recipe {
 
     @Id
     @GeneratedValue
-    private long id;
+    private Long id;
 
     @Column
     private String name;
@@ -22,11 +22,11 @@ public class Recipe {
     @ManyToOne
     private User user;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
