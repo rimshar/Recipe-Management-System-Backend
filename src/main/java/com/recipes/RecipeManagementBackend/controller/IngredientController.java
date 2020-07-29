496d97c2d47ae4dddf16176680948bd8b736a2cb
Raimonds Liepiņš <imb_lial@yahoo.com>
2020-07-29 22:44:53 +0300

    Add CrossOrigin annotation to IngredientController

--- src/main/java/com/recipes/RecipeManagementBackend/controller/IngredientController.java
+++ src/main/java/com/recipes/RecipeManagementBackend/controller/IngredientController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController("/api/rest/Ingredient.svc")
+@CrossOrigin(origins = "http://localhost:4200")
 public class IngredientController {
 
     private static Logger LOG = LoggerFactory.getLogger(RecipeManagementBackendApplication.class);
