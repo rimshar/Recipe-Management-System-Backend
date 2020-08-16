1c5f09da4e4fdf5e446638f2d3672df9cae0ec1f
Oshells <OskarssZarinss@gmail.com>
2020-08-16 11:08:35 +0300

    added save function to mesurement unit

--- src/main/java/com/recipes/RecipeManagementBackend/model/IngredientMeasurementUnit.java
+++ src/main/java/com/recipes/RecipeManagementBackend/model/IngredientMeasurementUnit.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 public class IngredientMeasurementUnit {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
     @Column
